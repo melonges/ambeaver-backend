01442326a771bdf98d5786806c1dd71b04b9235e
melonges <melonges@icloud.com>
2024-08-24 01:29:39 +0300

    fix: init CacheModule for DataBase module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,12 @@ import { SettingsModule } from './settings/settings.module';
 import { TasksModule } from './tasks/tasks.module';
 import { AssetsModule } from './assets/assets.module';
 import { SentryModule } from '@sentry/nestjs/setup';
+import { CacheModule } from './cache/cache.module';
 
 @Module({
   imports: [
     SentryModule.forRoot(),
+    CacheModule,
     AuthModule,
     TelegramModule,
     PlayerModule,
